b4168b591c171d199dc895ff8853879336ab0224
aronabot <gnl5672@gmail.com>
2021-04-21 19:51:47 +0900

    MOD: dynamic slick slider

--- scripts/event_builder.js
+++ scripts/event_builder.js
@@ -1,3 +1,24 @@
+let fs = require('fs');
+
+function buildEventSlider(eventDirectory){
+    var slickSlider = document.getElementsByClassName("slick-slider")
+    fs.readdir(eventDirectory, function(error, filelist){
+        for(var item in filelist){
+            var eventDiv = document.createElement("div"); 
+            eventDiv.setAttribute('class', 'event');
+
+            var eventImage = document.createElement("img");
+            eventImage.setAttribute('class', 'event');
+            eventImage.setAttribute('src', `${eventDirectory}/${item}`);
+            
+            eventDiv.appendChild(eventImage);
+            slickSlider.appendChild(eventDiv);
+        }
+    });
+}
+
+buildEventSlider('img/event')
+
 $('.slick-slider').slick({
     dots: true,
     dotsClass: "slick-dots",
