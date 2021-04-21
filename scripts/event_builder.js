d6178f807984903036814e93626958b7fe4f1fce
aronabot <gnl5672@gmail.com>
2021-04-21 20:16:32 +0900

    MOD: static slick slider

--- scripts/event_builder.js
+++ scripts/event_builder.js
@@ -1,30 +1,3 @@
-function buildEventSlider(requestURL){
-    var request = new XMLHttpRequest();
-
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-    
-    request.onload = function() {
-        var result = request.response;
-        var slickSlider = document.getElementsByClassName("slick-slider")
-        for(var idx in result){
-            var eventDiv = document.createElement("div"); 
-            eventDiv.setAttribute('class', 'event');
-    
-            var eventImage = document.createElement("img");
-            eventImage.setAttribute('class', 'event');
-            eventImage.setAttribute('src', result[idx]);
-            
-            eventDiv.appendChild(eventImage);
-            slickSlider.appendChild(eventDiv);
-        }
-
-    }
-}
-
-buildEventSlider('data/event.json')
-
 $('.slick-slider').slick({
     dots: true,
     dotsClass: "slick-dots",
