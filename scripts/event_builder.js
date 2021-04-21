1d233c3ce0e62a453a60697f1690ac70553c0a55
aronabot <gnl5672@gmail.com>
2021-04-21 19:10:19 +0900

    ADD: slick slider and logo

---
+++ scripts/event_builder.js
@@ -0,0 +1,14 @@
+$('.slick-slider').slick({
+    dots: true,
+    dotsClass: "slick-dots",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    variableWidth: true,
+    arrows: true,
+    //auto play property
+    autoplay: true,
+    autoplaySpeed: 5000,
+    infinity: true,
+    pauseOnHover: true
+});
