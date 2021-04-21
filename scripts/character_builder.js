54120d721394c1cd30a5012e2083b77b10d4b827
aronabot <gnl5672@gmail.com>
2021-04-21 22:41:40 +0900

    MOD: add info url

--- scripts/character_builder.js
+++ scripts/character_builder.js
@@ -6,7 +6,7 @@ function buildCharacterIcon(JSONObject){
         var characterPortrait = document.createElement("a");
         characterPortrait.setAttribute('class', 'character-portrait');
         //href append later
-        characterPortrait.setAttribute('href', `characters/${character['name']}`);
+        characterPortrait.setAttribute('href', `characters/${character['info']}`);
 
         var characterIcon = document.createElement("div");
         characterIcon.setAttribute('class', 'character-icon');
